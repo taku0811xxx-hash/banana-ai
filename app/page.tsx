060a99e5cb48ba768d0144a87c402d02ea998c06
林拓海 <hayashitakumi@hayashitakuminoMacBook-Pro-2.local>
2026-05-22 13:35:31 +0900

    fix: add type declaration for react-simple-maps

--- app/page.tsx
+++ app/page.tsx
@@ -311,7 +311,7 @@ export default function Home() {
 
       const data = await res.json();
 
-      setResult(JSON.parse(data.result));
+      setResult(data.result);
       analyzeSpots(image);
 
       // 前回のレシピをリセット
